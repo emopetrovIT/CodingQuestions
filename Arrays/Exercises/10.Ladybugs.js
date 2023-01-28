ee56680ebb5868ed8c8a2eb6a853a866d3c5f4eb
emopetrovIT <em.petrov88@gmail.com>
2023-01-28 22:25:56 +0200

    Fixed bug -  outer loop must continue to next command operation instead of breaking the cycle

--- Arrays/Exercises/10.Ladybugs.js
+++ Arrays/Exercises/10.Ladybugs.js
@@ -1,63 +1,52 @@
 function ladybugs(input) {
   const fieldSize = input[0];
-
-  let indexPositionsOfLadybugs = input[1].split(' ');
+  let ladybugsPositions = input[1].split(' ');
   const flyField = new Array(fieldSize).fill(0);
+  const isInRange = (index, array) => index < array.length && index >= 0;
 
-  for (let i = 0; i < indexPositionsOfLadybugs.length; i++) {
-    let currentLadybugIndexPosition = Number(indexPositionsOfLadybugs[i]);
+  for (let i = 0; i < ladybugsPositions.length; i++) {
+    let position = Number(ladybugsPositions[i]);
 
-    if (
-      currentLadybugIndexPosition >= flyField.length ||
-      currentLadybugIndexPosition < 0
-    ) {
+    if (!isInRange(position, flyField)) {
       continue;
     }
 
-    flyField[currentLadybugIndexPosition] = 1;
+    flyField[position] = 1;
   }
 
   let counter = 2;
-  let operation = input[counter];
   while (counter < input.length) {
-    operation = input[counter];
+    let operation = input[counter];
     let tokens = operation.split(' ');
-    let currentLadybugPosition = Number(tokens[0]);
+    let currentPosition = Number(tokens[0]);
     let direction = tokens[1];
     let flyLength = Number(tokens[2]);
+    let searchedLadybug = flyField[currentPosition];
+    let ladybugIsMissing = searchedLadybug === 0;
 
-    let ladybugLandOn = false;
-    let flyOutsideOfField = false;
-    // checks if index is valid
-    const isInRange = () => {
-      return (
-        currentLadybugPosition < flyField.length && currentLadybugPosition >= 0
-      );
-    };
-    // checks if given ladybug exist at given position
-    let givenPositionIsEmpty = flyField[currentLadybugPosition] === 0;
-
-    if (!isInRange() || givenPositionIsEmpty) {
-      break;
-    } else {
-      flyField[currentLadybugPosition] = 0;
+    if (!isInRange(currentPosition, flyField) || ladybugIsMissing) {
+      counter++;
+      continue;
     }
 
-    while (!ladybugLandOn && !flyOutsideOfField) {
+    flyField[currentPosition] = 0;
+
+    let trackingLadybugFlight = true;
+    while (trackingLadybugFlight) {
       if (direction === 'left') {
-        currentLadybugPosition -= flyLength;
+        currentPosition -= flyLength;
       } else if (direction === 'right') {
-        currentLadybugPosition += flyLength;
+        currentPosition += flyLength;
       }
 
-      if (!isInRange()) {
-        flyOutsideOfField = true;
+      if (!isInRange(currentPosition, flyField)) {
+        trackingLadybugFlight = false;
         break;
       }
 
-      if (flyField[currentLadybugPosition] === 0) {
-        flyField[currentLadybugPosition] = 1;
-        ladybugLandOn = true;
+      if (flyField[currentPosition] === 0) {
+        flyField[currentPosition] = 1;
+        trackingLadybugFlight = false;
       }
     }
 
@@ -67,7 +56,6 @@ function ladybugs(input) {
   console.log(flyField.join(' '));
 }
 
-// ladybugs([3, '0 1', '0 right 1', '2 right 1']);
-// ladybugs([5, '3', '3 left 2', '1 left -2']);
-// ladybugs([5, '3 -5 -10', '3 left 2', '1 left -2']);
-ladybugs([10, '2 3 4', '2 right 2']);
+ladybugs([3, '0 1', '0 right 1', '2 right 1']);
+ladybugs([5, '3 -5 -10', '3 left 2', '1 left -2']);
+ladybugs([5, '2', '2 left -1']); // in case flylength = 0, the ladybug position should be set to 0;
